ca15aaf17516e9095b5de5447244bdfcb5cb0c2c
David Wilson <calxdesign@gmail.com>
2018-12-11 08:33:32 +0000

    Sphere no normals

--- Horus/src/Horus.cpp
+++ Horus/src/Horus.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <windows.h>
+#include "vec3.h"
 
 typedef signed char           s8;
 typedef char                  u8;
@@ -12,8 +13,8 @@ typedef unsigned long long    u64;
 typedef float				  f32;
 typedef double				  f64;
 
-const static u32 output_width	= 1920;
-const static u32 output_height	= 1080;
+const static u32 output_width	= 640;
+const static u32 output_height	= 320;
 const static u32 output_size	= output_width * output_height;
 
 #pragma pack(1)
@@ -42,6 +43,39 @@ struct BitmapInfoHeader
 	u32 colours_important;
 };
 
+struct Ray
+{
+	vec3 origin;
+	vec3 direction;
+};
+
+bool sphere(vec3& centre, float radius, Ray r)
+{
+	vec3	oc = r.origin - centre;
+	f32		a = dot(r.direction, r.direction);
+	f32		b = 2.0f * dot(oc, r.direction);
+	f32		c = dot(oc, oc) - radius * radius;
+	f32		d = b*b - 4*a*c;
+
+	return (d > 0);
+}
+
+vec3 colour(Ray& r)
+{
+	
+	vec3	sphere_colour = vec3(1.0f, 0.0f, 0.0f);
+	vec3	sphere_position = vec3(0.0f, 0.0f, -1.0f);
+	f32		sphere_radius = 0.5f;
+
+	vec3	dir = normalize(r.direction);
+	f32		t = 0.5f * (dir.y() + 1.0f);
+	vec3	sky_colour = (1.0f - t) * vec3(1.0f, 1.0f, 1.0f) + t * vec3(0.5f, 0.7f, 1.0f);
+
+	vec3 final = sphere(sphere_position, sphere_radius, r) ? sphere_colour : sky_colour;
+
+	return final;
+}
+
 int main()
 {
 	u32 remainder = output_width % 4;
@@ -64,25 +98,35 @@ int main()
 	info_header.bits = 24;
 	info_header.compression = 0;
 	info_header.image_size = row_size * output_height;
-	info_header.x_resolution = output_width;
-	info_header.y_resolution = output_height;
+	info_header.x_resolution = 0;
+	info_header.y_resolution = 0;
 	info_header.colours = 0;
 	info_header.colours_important = 0;
 
 	u8* bitmap_image_data = (u8*) malloc(info_header.image_size);
 	u32 bitmap_index = 0;
 
+	vec3 bottom_left(-2.0f, -1.0f, -1.0f);
+	vec3 horizontal	( 4.0f,  0.0f,  0.0f);
+	vec3 vertical	( 0.0f,  2.0f,  0.0f);
+	vec3 origin		( 0.0f,  0.0f,  0.0f);
+
 	for (s16 y = 0; y < output_height; y++)
 	{
 		for (s16 x = 0; x < output_width; x++)
 		{
-			f32 r = (f32) x / (f32) output_width;
-			f32 g = (f32) y / (f32) output_height;
-			f32 b = 0.2f;
+			f32 u = (f32) x / (f32) output_width;
+			f32 v = (f32) y / (f32) output_height;
 
-			u8 red =  (int) 255.99 * r;
-			u8 grn =  (int) 255.99 * g;
-			u8 blu =  (int) 255.99 * b;
+			Ray r;
+			r.origin = origin;
+			r.direction = bottom_left + u * horizontal + v * vertical;
+
+			vec3 c = colour(r);
+
+			u8 red =  (int) 255.99 * c.r();
+			u8 grn =  (int) 255.99 * c.g();
+			u8 blu =  (int) 255.99 * c.b();
 
 			bitmap_image_data[bitmap_index] = blu;
 			bitmap_index++;
@@ -95,7 +139,7 @@ int main()
 		}
 	}
 
-	FILE* file = fopen("C:\\Users\\dave.wilson\\Desktop\\output.bmp", "wb");
+	FILE* file = fopen("C:\\Users\\dave.wilson\\Documents\\Dave\\Horus\\Renders\\output.bmp", "wb");
 
 	if (!file)
 	{
@@ -108,6 +152,4 @@ int main()
 	fwrite(bitmap_image_data,	info_header.image_size,		1, file);
 
 	fclose(file);
-
-	printf("");
 }
