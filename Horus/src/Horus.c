341a45330a424fee71179722e9f336455dabb94e
David Wilson <calxdesign@gmail.com>
2019-02-11 19:42:18 +0000

    Finished with this

--- Horus/src/Horus.c
+++ Horus/src/Horus.c
@@ -10,10 +10,10 @@ typedef signed char			s8;
 typedef char				u8;
 typedef short				s16;
 typedef unsigned short			u16;
-typedef int						s32;
-typedef unsigned int	u32;
-typedef long long				s64;
-typedef unsigned long long	u64;
+typedef int				s32;
+typedef unsigned int		u32;
+typedef long long			s64;
+typedef unsigned long long		u64;
 typedef float				f32;
 typedef double				f64;
 
