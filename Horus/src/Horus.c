627af90d883fd35bdbeedac4f237d440889ccd04
David Wilson <calxdesign@gmail.com>
2019-02-11 19:41:30 +0000

    Finished with this

--- Horus/src/Horus.c
+++ Horus/src/Horus.c
@@ -11,11 +11,11 @@ typedef char				u8;
 typedef short				s16;
 typedef unsigned short			u16;
 typedef int						s32;
-typedef unsigned int			u32;
+typedef unsigned int	u32;
 typedef long long				s64;
-typedef unsigned long long		u64;
-typedef float					f32;
-typedef double					f64;
+typedef unsigned long long	u64;
+typedef float				f32;
+typedef double				f64;
 
 #define FINISHED_MESSAGE		
 #define NIGHT
